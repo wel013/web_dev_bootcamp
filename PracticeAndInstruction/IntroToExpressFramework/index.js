4c5dc357b8156a56e3da634849c5017d01cc445e
Wenqian Li <91103791+wel013@users.noreply.github.com>
2024-08-09 18:27:51 -0400

    Update index.js

--- PracticeAndInstruction/IntroToExpressFramework/index.js
+++ PracticeAndInstruction/IntroToExpressFramework/index.js
@@ -1,5 +1,3 @@
-//To see how the final website should work, run "node solution.js".
-//Make sure you have installed all the dependencies with "npm i".
 //The password is ILoveProgramming
 import express from "express";
 import { dirname } from "path";
