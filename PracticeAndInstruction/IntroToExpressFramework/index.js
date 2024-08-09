0db2d86e263b6fef30f1692edcde089a1ee02c33
Wenqian Li <91103791+wel013@users.noreply.github.com>
2024-08-09 18:24:39 -0400

    Add files via upload

---
+++ PracticeAndInstruction/IntroToExpressFramework/index.js
@@ -0,0 +1,45 @@
+//To see how the final website should work, run "node solution.js".
+//Make sure you have installed all the dependencies with "npm i".
+//The password is ILoveProgramming
+import express from "express";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+var userAuthenticated = false;
+
+function checkPassword(req, res, next) {
+    var user = (req.body["password"]);
+    if (user === "ILoveProgramming") {
+        userAuthenticated = true;
+    }
+    //because we are creating an middle ware
+    next(); // we need this to allow the flow
+}
+app.use(checkPassword)
+app.get("/", (req, res) => {
+    //require the exact path
+    res.sendFile(__dirname + "/public/index.html");
+});
+
+app.post("/check", (req, res) => {
+    if (userAuthenticated) {
+        res.sendFile(__dirname + "/public/secret.html");
+    }
+    else {
+        // res.sendFile(__dirname + "/public/index.html");
+        res.redirect("/")
+    }
+})
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
